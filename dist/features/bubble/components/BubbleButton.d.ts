151ba2681c243254ff574556e7b2ffbfe3865ffa
Raúl Camacho <raulj.camacho@gmail.com>
2024-01-22 17:42:35 -0500

    Delete unused files

--- dist/features/bubble/components/BubbleButton.d.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonTheme, ChatWindowTheme } from '../types';
-type Props = ButtonTheme & ChatWindowTheme & {
-    isBotOpened: boolean;
-    toggleBot: () => void;
-};
-export declare const BubbleButton: (props: Props) => import("solid-js").JSX.Element;
-export {};
-//# sourceMappingURL=BubbleButton.d.ts.map
