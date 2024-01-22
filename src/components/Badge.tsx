13b043053d2e89b701cc7c364d7ccf067632bc46
Raúl Camacho <raulj.camacho@gmail.com>
2024-01-22 11:29:35 -0500

    Remove Branding and Change icon Refresh

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Desarrollado por
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://neuronai.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> NeuronAI</span>
       </a>
     </span>
   );
--- src/components/icons/DeleteIcon.tsx
+++ src/components/icons/DeleteIcon.tsx
@@ -3,7 +3,7 @@ const defaultButtonColor = '#3B81F6';
 export const DeleteIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon-tabler icon-tabler-refresh"
+    class="icon icon-tabler icon-tabler-eraser"
     width="24"
     height="24"
     viewBox="0 0 24 24"
