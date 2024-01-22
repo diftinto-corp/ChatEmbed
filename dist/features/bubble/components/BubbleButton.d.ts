053e90800b6d5a6cfd4cbd323802ae9f6e074d0b
Raúl Camacho <raulj.camacho@gmail.com>
2024-01-22 17:44:26 -0500

    Refactor code to improve performance and readability

---
+++ dist/features/bubble/components/BubbleButton.d.ts
@@ -0,0 +1,8 @@
+import { ButtonTheme, ChatWindowTheme } from '../types';
+type Props = ButtonTheme & ChatWindowTheme & {
+    isBotOpened: boolean;
+    toggleBot: () => void;
+};
+export declare const BubbleButton: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=BubbleButton.d.ts.map
